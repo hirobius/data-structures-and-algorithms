eaa82d7f15c0a63abafeeada70920f566459dd89
Adrian Milsap <adrian@hirobius.com>
2021-04-26 21:23:17 -0700

    partway

--- code-challenges/challenges-10.test.js
+++ code-challenges/challenges-10.test.js
@@ -64,8 +64,15 @@ For example:
 
 return: 35
 ------------------------------------------------------------------------------------------------ */
+// failing... ???
 const totalSum = (matrix) => {
-  // Solution code here...
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      sum = sum + matrix[i][j];
+    }
+  }
+  return sum;
 };
 
 
